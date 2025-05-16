0a4f00ef5faceae7f1867e49da4a815d24bb1137
Adnan <azizulislamadnan@gmail.com>
2025-05-16 11:29:11 +0600

    add: prod

--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -5,7 +5,17 @@ const getDetailsPost = async (id) => {
   const data = await res.json();
   return data;
 };
-
+export const generateMetadata = async ({ params }) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.id}`
+  );
+  const postData = await res.json();
+  return {
+    title: postData?.title,
+    description: postData?.body,
+    keywords: postData?.body?.split(" "),
+  };
+};
 const page = async ({ params }) => {
   const { title, body } = await getDetailsPost(params.id);
   return (
